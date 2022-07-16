f76e5d7d3e49764dad42eb72e34a9ef7ee9a789b
佐藤太一 <3101taichi@gmail.com>
2022-07-16 09:16:07 +0900

    queryを呼び出せるように

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, createContext } from "react"
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
+import Cookies from "js-cookie"
+import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from "@apollo/client"
+import { setContext } from '@apollo/client/link/context';
+
 import CommonLayout from "components/layouts/CommonLayout"
 import Home from "components/pages/Home"
 import SignUp from "components/pages/SignUp"
@@ -49,6 +53,35 @@ const App: React.FC = () => {
     handleGetCurrentUser()
   }, [setCurrentUser])
 
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3333/graphql',
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      "client": Cookies.get("_client"),
+      "uid": Cookies.get("_uid")
+    }
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const token = Cookies.get("_access_token")
+    const client = Cookies.get("_client")
+    const uid = Cookies.get("_uid")
+    return {
+      headers: {
+        ...headers,
+        accessToken: token,
+        client: client,
+        uid: uid
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    // link: httpLink,
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink)
+  })
+
 
   // ユーザーが認証済みかどうかでルーティングを決定
   // 未認証だった場合は「/signin」ページに促す
@@ -67,15 +100,17 @@ const App: React.FC = () => {
   return (
     <Router>
       <AuthContext.Provider value={{ loading, setLoading, isSignedIn, setIsSignedIn, currentUser, setCurrentUser}}>
-        <CommonLayout>
-          <Switch>
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/signin" component={SignIn} />
-            <Private>
-              <Route exact path="/" component={Home} />
-            </Private>
-          </Switch>
-        </CommonLayout>
+        <ApolloProvider client={client}>
+          <CommonLayout>
+            <Switch>
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/signin" component={SignIn} />
+              <Private>
+                <Route exact path="/" component={Home} />
+              </Private>
+            </Switch>
+          </CommonLayout>
+        </ApolloProvider>
       </AuthContext.Provider>
     </Router>
   )
